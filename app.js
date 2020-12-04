0cea908847ef4dc09ebfca5be24c9b44c33faac1
mmoya26 <mmoya18@icloud.com>
2020-12-04 01:02:28 -0600

    Changed the whole app functionality to a different form of usability

--- app.js
+++ app.js
@@ -6,33 +6,19 @@ btn_reset.addEventListener("click", reset);
 
 let input_div_container = document.getElementById("inputs_container");
 
-let inputSizeFieldElement = document.getElementById("input_size_field");
+let containerNodesCount = input_div_container.childElementCount;
 
 function generate() {
-    
-    let inputSizeValue = inputSizeFieldElement.value;
+    containerNodesCount = input_div_container.childElementCount + 1;
+    let inputBox = document.createElement("input");
 
-    // CHECK IF THE USER LEFT THE FIELD EMPTY OR ENTERED 0
-    // IF SO RETURN AND DO NOT GENERATE INPUT BOXES
-    if (inputSizeValue == "" || inputSizeValue == 0) {
-        inputSizeValue = 0
-        console.log("Input value: O or Empty")
-        return
-    }
-    console.log("Input value: " + inputSizeValue)
-
-    for (i = 0; i < inputSizeValue; i++) {
-        var inputBox = document.createElement("input");
-        inputBox.setAttribute("class", "input_square");
-        inputBox.setAttribute("maxlength", "1");
-        input_div_container.appendChild(inputBox);
-    }
-
-    disableGenerateButton()
+    inputBox.setAttribute("class", "input_square");
+    inputBox.setAttribute("maxlength", "1");
+    inputBox.setAttribute("id", `input_box_${containerNodesCount}`);
+    input_div_container.appendChild(inputBox);
 }
 
 function reset() {
-    btn_generate.disabled = false;
 
     while(input_div_container.firstChild) {
         input_div_container.removeChild(input_div_container.firstChild)
@@ -40,8 +26,4 @@ function reset() {
 
     inputSizeFieldElement.value = "";
     inputSizeFieldElement.focus();
-}
-
-function disableGenerateButton() {
-    btn_generate.disabled = true;
 }
