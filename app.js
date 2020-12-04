902158d33bbc44ebb83b84b3c89844178803ae1a
mmoya26 <mmoya18@icloud.com>
2020-12-04 12:05:04 -0600

    Add automatically generated table + Allow to selected increment variable +  New css styles

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 let btn_add = document.getElementById("btn_add")
 let input_div_container = document.getElementById("inputs_container");
 let containerNodesCount = input_div_container.childElementCount;
+let choosenCharacter;
+let generatedTable = document.getElementById("generated_table");
+let alreadyChoosen = false
 
 btn_add.addEventListener("click", (event) =>  {
     containerNodesCount = input_div_container.childElementCount + 1;
@@ -23,15 +26,59 @@ btn_add.addEventListener("click", (event) =>  {
             nextElement.focus();
         }
     });
+
+    inputBox.addEventListener("dblclick", (event) => {
+        console.log("Double clicked...");
+
+        if (alreadyChoosen) {
+            if (event.target.classList.contains("selected")) {
+                inputBox.classList.remove("selected");
+                alreadyChoosen = false;
+            }
+            return
+        }
+
+        if (inputBox.classList.contains("selected")) {
+            inputBox.classList.remove("selected");
+            alreadyChoosen = false;
+        }   
+        else {
+            inputBox.classList.add("selected");
+            alreadyChoosen = true;
+        }
+        
+    });
+
     input_div_container.appendChild(inputBox);
 });
 
 let btn_reset = document.getElementById("btn_reset");
 
 btn_reset.addEventListener("click", (event) => {
+
+    // REMOVE ALL INPUT BOXES
     while(input_div_container.firstChild) {
         input_div_container.removeChild(input_div_container.firstChild)
     }
+
+    // REMOVE ALL TABLE ROWS
+    while(generatedTable.firstChild) {
+        generatedTable.removeChild(generatedTable.firstChild);
+    }
 });
 
+
+let btn_generate = document.getElementById("btn_generate");
+btn_generate.addEventListener("click", (event) => {
+    let counter = 5;
+    
+
+    for (i = 0; i < counter; i++) {
+        let tableRow = document.createElement("tr") 
+        let tableData = document.createElement("td");
+        tableData.innerHTML = "XD";
+        tableRow.appendChild(tableData);
+        generatedTable.appendChild(tableRow);
+    }
+});
 
