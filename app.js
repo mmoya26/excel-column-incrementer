08ae213785aa6c02e15bb5d46cc2f0273d30ccc1
mmoya26 <mmoya18@icloud.com>
2020-12-04 01:25:01 -0600

    Unfinished focus input boxes functionality

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 let btn_generate = document.getElementById("btn_generate")
+let input_div_container = document.getElementById("inputs_container");
+let containerNodesCount = input_div_container.childElementCount;
 
 btn_generate.addEventListener("click", (event) =>  {
     containerNodesCount = input_div_container.childElementCount + 1;
@@ -7,6 +9,11 @@ btn_generate.addEventListener("click", (event) =>  {
     inputBox.setAttribute("class", "input_square");
     inputBox.setAttribute("maxlength", "1");
     inputBox.setAttribute("id", `input_box_${containerNodesCount}`);
+    inputBox.addEventListener("input", (event) => {
+        let idNumber = parseInt(inputBox.getAttribute("id").replace( /^\D+/g, ''), 10) + 1;
+        let nextElement = document.getElementById(`input_box_${idNumber}`);
+        nextElement.focus();
+    })
     input_div_container.appendChild(inputBox);
 });
 
@@ -20,6 +27,4 @@ btn_reset.addEventListener("click", (event) => {
     inputSizeFieldElement.focus();
 });
 
-let input_div_container = document.getElementById("inputs_container");
 
-let containerNodesCount = input_div_container.childElementCount;
