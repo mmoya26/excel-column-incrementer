48f5356c04064e74f75032e09c5174053d05e6df
mmoya26 <mmoya18@icloud.com>
2020-12-04 11:05:08 -0600

    Finish focus input boxes functionality

--- app.js
+++ app.js
@@ -10,21 +10,28 @@ btn_generate.addEventListener("click", (event) =>  {
     inputBox.setAttribute("maxlength", "1");
     inputBox.setAttribute("id", `input_box_${containerNodesCount}`);
     inputBox.addEventListener("input", (event) => {
+
+        // CHECK IF THE CHANGE WAS SIMPLY A BACKSPACE
+        // IF SO THEN RETURN BECAUSE WE DON'T WANT TO REMOVE FOCUS FROM CURRENT INPUT BOX 
+        if (event.data == null) {
+            return
+        }
+
         let idNumber = parseInt(inputBox.getAttribute("id").replace( /^\D+/g, ''), 10) + 1;
         let nextElement = document.getElementById(`input_box_${idNumber}`);
-        nextElement.focus();
-    })
+        if (nextElement) {
+            nextElement.focus();
+        }
+    });
     input_div_container.appendChild(inputBox);
 });
 
 let btn_reset = document.getElementById("btn_reset");
+
 btn_reset.addEventListener("click", (event) => {
     while(input_div_container.firstChild) {
         input_div_container.removeChild(input_div_container.firstChild)
     }
-
-    inputSizeFieldElement.value = "";
-    inputSizeFieldElement.focus();
 });
 
 
